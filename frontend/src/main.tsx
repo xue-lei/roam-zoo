54f35698649d6c17b36bf9c4028575779519c50d
xuelei <xuelei@ieforever.com>
2024-01-07 19:26:27 +0800

    add unocss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './style.css'
+import 'uno.css'
+import 'virtual:unocss-devtools'
 import App from './App'
 
 const container = document.getElementById('root')
@@ -9,6 +11,6 @@ const root = createRoot(container!)
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 )
